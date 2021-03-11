0f5cf2047048e1c5ccbef27169acf71d66478849
Conrad <vibrantclouds@protonmail.com>
2021-03-11 00:09:46 -0500

    MangaLife version inc

--- src/MangaLife/MangaLife.ts
+++ src/MangaLife/MangaLife.ts
@@ -19,7 +19,7 @@ const headers = { "content-type": "application/x-www-form-urlencoded" }
 const method = 'GET'
 
 export const MangaLifeInfo: SourceInfo = {
-  version: '2.1.2',
+  version: '2.1.3',
   name: 'Manga4Life',
   icon: 'icon.png',
   author: 'Daniel Kovalevich',
