2647778ac09547e04776b0223b2b429270fd526c
ConradWeiser <vibrantclouds@protonmail.com>
2021-03-11 05:04:24 +0000

    Update gh-pages to output generated at f693b60

--- MangaLife/source.js
+++ MangaLife/source.js
@@ -342,7 +342,7 @@ exports.ML_DOMAIN = 'https://manga4life.com';
 const headers = { "content-type": "application/x-www-form-urlencoded" };
 const method = 'GET';
 exports.MangaLifeInfo = {
-    version: '2.1.1',
+    version: '2.1.2',
     name: 'Manga4Life',
     icon: 'icon.png',
     author: 'Daniel Kovalevich',
@@ -564,7 +564,7 @@ exports.parseChapterDetails = (data, mangaId, chapterId) => {
     // Sometimes, Manga4Life swaps between vm.CurPathName and vm.CurPathNames - Always get whichever one is currently populated at the moment
     let matchedPath = (_a = data.match(/vm.CurPathName = (.*);/)) === null || _a === void 0 ? void 0 : _a[1];
     if (!matchedPath) {
-        matchedPath = (_b = data.match(/vm.CurPathNames = (.*);/)) === null || _b === void 0 ? void 0 : _b[1];
+        matchedPath = (_b = data.match(/vm.CurPathNames = "(.*)";/)) === null || _b === void 0 ? void 0 : _b[1];
     }
     const chapterInfo = JSON.parse((_c = data.match(/vm.CurChapter = (.*);/)) === null || _c === void 0 ? void 0 : _c[1]);
     const pageNum = Number(chapterInfo.Page);
