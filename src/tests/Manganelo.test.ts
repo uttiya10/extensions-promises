3ed532bb7123ed5f756e3f56b0763e03936f6ea0
Narin <narin.tana@hotmail.com>
2021-06-04 22:14:27 +0700

    Updated Manganelo's Rebranding to Manganato (#261)
    * Updated Manganelo
    
    * Update src/Manganelo/ManganeloParser.ts
    
    Co-authored-by: Conrad Weiser <vibrantclouds@protonmail.com>
    
    * Throw error when cant get new mangaId
    
    * Update src/Manganelo/ManganeloParser.ts
    
    Co-authored-by: Conrad Weiser <vibrantclouds@protonmail.com>
    
    * Update src/Manganelo/ManganeloParser.ts
    
    Co-authored-by: Conrad Weiser <vibrantclouds@protonmail.com>
    
    * Update ManganeloParser.ts
    
    - Added checks to see if .parent() or .next() are null
    - Fixed tag ids
    - Added failback images to areas needed
    
    * Small fixes
    
    - Replace old replace methods (using domain/manga)
    - Added id checking for chapters and updates
    - Throw error is image are null
    - Added missing semicolon :D
    Thanks Vibrant!
    
    Co-authored-by: Conrad Weiser <vibrantclouds@protonmail.com>
    Co-authored-by: TheNetsky <56271887+TheNetsky@users.noreply.github.com>

--- src/tests/Manganelo.test.ts
+++ src/tests/Manganelo.test.ts
@@ -15,7 +15,7 @@ describe('Manganelo Tests', function () {
      * Try to choose a manga which is updated frequently, so that the historical checking test can 
      * return proper results, as it is limited to searching 30 days back due to extremely long processing times otherwise.
      */
-    var mangaId = "zt922734";   // Mashle
+    var mangaId = "zt922734"; // Legacy Mashle
 
     it("Retrieve Manga Details", async () => {
         let details = await wrapper.getMangaDetails(source, mangaId);
